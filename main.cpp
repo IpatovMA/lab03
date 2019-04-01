28b4d33407f4d5a28519c0b69a77720eefa1cd59
IaptovMA <mail@mail.mail>
2019-04-01 10:22:15 +0300

    code:добавлена функция расчета гистограммы

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ vector<double> input_numbers(size_t count) {
 }
 
 void
-find_minmax(vector<double> numbers, double* min, double* max) {
+find_minmax(const vector<double>& numbers, double* min, double* max) {
     *min = numbers[0];
     *max = numbers[0];
     for (double number : numbers) {
@@ -23,6 +23,20 @@ find_minmax(vector<double> numbers, double* min, double* max) {
             *max = number;
         }
     }
+}
+vector<size_t> make_histogram(const vector<double>& numbers, size_t& bin_count){
+    vector<size_t> bins(bin_count);
+    double min, max;
+    min = max = numbers[0];
+    find_minmax(numbers, &min, &max);
+    for (double number : numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count) {
+            bin--;
+        }
+        bins[bin]++;
+    }
+    return bins;
 }
 
 
@@ -40,17 +54,7 @@ main() {
     cin >> bin_count;
 
     // Обработка данных
-    double min, max;
-    find_minmax(numbers, &min, &max);
-
-    vector<size_t> bins(bin_count);
-    for (double number : numbers) {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count) {
-            bin--;
-        }
-        bins[bin]++;
-    }
+    const auto bins = make_histogram(numbers, bin_count);
 
     // Вывод данных
     const size_t SCREEN_WIDTH = 80;
