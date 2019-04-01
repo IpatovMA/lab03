cfcdba5fac019bc94dac667a7c7d57b91b67ff78
IaptovMA <mail@mail.mail>
2019-04-01 12:46:03 +0300

    добавлены модули, функция минмакс вынесена в модуль

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include "histogram.h"
 using namespace std;
 
 const vector<double> input_numbers(size_t count)
@@ -13,29 +13,13 @@ const vector<double> input_numbers(size_t count)
     return result;
 }
 
-void
-find_minmax(const vector<double>& numbers, double* min, double* max)
-{
-    *min = numbers[0];
-    *max = numbers[0];
-    for (double number : numbers)
-    {
-        if (number < *min)
-        {
-            *min = number;
-        }
-        if (number > *max)
-        {
-            *max = number;
-        }
-    }
-}
+
 const vector<size_t> make_histogram(const vector<double>& numbers, size_t& bin_count)
 {
     vector<size_t> bins(bin_count);
     double min, max;
     min = max = numbers[0];
-    find_minmax(numbers, &min, &max);
+    find_minmax(numbers, min, max);
     for (double number : numbers)
     {
         size_t bin = (size_t)((number - min) / (max - min) * bin_count);
