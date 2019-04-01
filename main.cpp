6ce5901bd18169dc36a48f58ce373139356c9b30
IaptovMA <u211-19@public.mpei.local>
2019-04-01 09:41:13 +0300

    code:добавлена функция ввода;number ->const

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+vector<double> input_numbers(size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        cin >> result[i];
+    }
+    return result;
+}
+
 int
 main() {
     // ¬вод данных
