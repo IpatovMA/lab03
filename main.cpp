108dab82d4154867c44f385faf6a6af1553cd0c8
IaptovMA <mail@mail.mail>
2019-04-01 09:51:32 +0300

    code:добавлена функция findminmax

--- main.cpp
+++ main.cpp
@@ -11,34 +11,37 @@ vector<double> input_numbers(size_t count) {
     return result;
 }
 
+void
+find_minmax(vector<double> numbers, double* min, double* max) {
+    *min = numbers[0];
+    *max = numbers[0];
+    for (double number : numbers) {
+        if (number < *min) {
+            *min = number;
+        }
+        if (number > *max) {
+            *max = number;
+        }
+    }
+}
+
+
 int
 main() {
     // ¬вод данных
-    size_t number_count;
     cerr << "Enter number count: ";
-    cin >> number_count;
 
-    cerr << "Enter numbers: ";
-    vector<double> numbers(number_count);
-    for (size_t i = 0; i < number_count; i++) {
-        cin >> numbers[i];
-    }
+    size_t number_count;
+    cin >> number_count;
+    const auto numbers = input_numbers(number_count);
 
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
 
     // ќбработка данных
-    double min = numbers[0];
-    double max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
+    double min, max;
+    find_minmax(numbers, &min, &max);
 
     vector<size_t> bins(bin_count);
     for (double number : numbers) {
